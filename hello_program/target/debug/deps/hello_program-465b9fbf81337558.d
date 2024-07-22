5d086068c4e0201e28d617c554d4b73bb9fbc481
jaikumar9 <Jaiagdayemawer556@gmail.com>
2024-07-22 18:06:48 +0530

    Initial learning of Rust

---
+++ hello_program/target/debug/deps/hello_program-465b9fbf81337558.d
@@ -0,0 +1,5 @@
+C:\Users\DELL_5\Desktop\RustLearnings\hello_program\target\debug\deps\libhello_program-465b9fbf81337558.rmeta: src\main.rs
+
+C:\Users\DELL_5\Desktop\RustLearnings\hello_program\target\debug\deps\hello_program-465b9fbf81337558.d: src\main.rs
+
+src\main.rs:
---
+++ hello_program/target/debug/deps/hello_program-d3f6dfdf577f1271.d
@@ -0,0 +1,5 @@
+C:\Users\DELL_5\Desktop\RustLearnings\hello_program\target\debug\deps\libhello_program-d3f6dfdf577f1271.rmeta: src\main.rs
+
+C:\Users\DELL_5\Desktop\RustLearnings\hello_program\target\debug\deps\hello_program-d3f6dfdf577f1271.d: src\main.rs
+
+src\main.rs:
