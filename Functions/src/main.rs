704da80a1d7bca93469afc5c23d6f064d041ce84
jaikumar9 <Jaiagdayemawer556@gmail.com>
2024-09-18 14:48:27 +0530

    Add in functions folder

--- Functions/src/main.rs
+++ Functions/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
     println!("Hello, Functions!");
+
+    another_function_without_arg();
+    three_function();
+    another_function(5);
+    print_labaled_mesurement(42,'L');
+}
+
+
+fn another_function_without_arg() {
+    println!("Another function.");
+}
+
+fn three_function() {
+    println!("3rd function")
+}
+
+fn another_function(x: u32) {
+    println!("the value of x is {}", x);
+}
+
+fn print_labaled_mesurement(value:u32, unit_lable: char){
+    print!("The measurement is: {} {}", value, unit_lable);
 }
---
+++ Functions/target/debug/deps/Functions-e85c647b0b6b55d6.d
@@ -0,0 +1,5 @@
+C:\Users\DELL_5\Desktop\Rust_Learnings\Functions\target\debug\deps\libFunctions-e85c647b0b6b55d6.rmeta: src\main.rs
+
+C:\Users\DELL_5\Desktop\Rust_Learnings\Functions\target\debug\deps\Functions-e85c647b0b6b55d6.d: src\main.rs
+
+src\main.rs:
